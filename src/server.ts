4eea08f7c39b80f1e8208f998414cfc4aece65f4
take0420 <zakisen81@gmail.com>
2025-02-04 22:50:16 +0900

    feat: Todoを追加する機能を実装

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,54 @@
 import { ApolloServer, gql } from 'apollo-server';
 
+const todos = [
+  {
+    id: '1',
+    title: 'GraphQLを学習する',
+    completed: false,
+  },
+  {
+    id: '2',
+    title: 'Reactを学習する',
+    completed: false,
+  },
+];
+
 const typeDefs = gql`
+  type Todo {
+    id: ID!
+    title: String!
+    completed: Boolean!
+  }
+
   type Query {
-    message: String
+    getTodos: [Todo!]!
+  }
+
+  type Mutation {
+    addTodo(title: String!): Todo!
   }
 `;
 
+type AddTodo = {
+  title: string;
+};
+
 const resolvers = {
   Query: {
-    message: () => 'Hello World!',
+    getTodos: () => todos,
+  },
+
+  Mutation: {
+    addTodo: (_: unknown, { title }: AddTodo) => {
+      const newTodo = {
+        id: String(todos.length + 1),
+        title,
+        completed: false,
+      };
+
+      todos.push(newTodo);
+      return newTodo;
+    },
   },
 };
 
